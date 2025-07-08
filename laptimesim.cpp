6129218e7bfc9f7941be4d7889a90a01e7eb3149
gromer <gromer@student.chalmers.se>
2025-07-08 15:22:56 +0200

    first commit

---
+++ laptimesim.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+
+class Car{
+    public:
+        float mass;
+        std::vector<float> hardpoints {0,0,0,0,0,0,0,0};
+        
+    void load_data(){
+        
+    }
+};
+
+int main(){
+
+
+    return 0;
+}
