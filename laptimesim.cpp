56630de56c422a207bcc3bfef5aed8265dae4ff1
gromer <gromer@student.chalmers.se>
2025-07-08 17:14:32 +0200

    Added load data method to Car-class.

--- laptimesim.cpp
+++ laptimesim.cpp
@@ -1,14 +1,34 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
-
+#include <sstream>
 
 class Car{
     public:
         float mass;
-        std::vector<float> hardpoints {0,0,0,0,0,0,0,0};
+        float cL;
+        float cD;
+        float mu;
+        std::vector<float> hardpoints;
         
-    void load_data(){
-        
+    void load_data(const std::string& filename){
+        std::ifstream file(filename);
+        std::string line;
+
+        while (std::getline(file, line)) {
+            std::stringstream ss(line);
+            std::string item;
+            std::vector<std::string> row;
+
+            while (std::getline(ss, item, ',')) {
+                row.push_back(item);
+            }
+
+            for (const auto& cell : row) {
+                std::cout << cell << " ";
+            }
+            std::cout << std::endl;
+        }
     }
 };
 
